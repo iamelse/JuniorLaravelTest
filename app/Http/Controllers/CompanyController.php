11957cd75f9b6e5448acabab5e9802dcdd38ffe5
Lana Septiana <lanaseptiana2@gmail.com>
2022-03-03 12:59:30 +0700

    refactor: removing comment code from CompanyController & EmployeeController

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -14,11 +14,6 @@ class CompanyController extends Controller
         $this->middleware('auth');
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return view('company.index', [
@@ -26,22 +21,11 @@ class CompanyController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('company.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $validated = $request->validate([
@@ -60,12 +44,6 @@ class CompanyController extends Controller
         return redirect('/company')->with('success', '<strong>Success!</strong> New post has been created!');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $company = Company::find($id);
@@ -74,12 +52,6 @@ class CompanyController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         $company = Company::find($id);
@@ -88,13 +60,6 @@ class CompanyController extends Controller
         ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $validated = $request->validate([
@@ -115,12 +80,6 @@ class CompanyController extends Controller
         return redirect('/company')->with('success', '<strong>Success!</strong> Post has been updated!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $company = Company::find($id);
